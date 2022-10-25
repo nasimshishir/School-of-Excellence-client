6d1cf8e76d5e67f81d94fe55d69776d29fcbce4a
nasimshishir <tech17196@gmail.com>
2022-10-25 23:47:37 +0600

    initialling first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
+  const notify = () => toast('Here is your toast.');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={notify} className="|">Make me a toast</button>
+      <Toaster />
     </div>
   );
 }
