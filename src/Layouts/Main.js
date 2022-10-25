f57e7c233469c45d3db3d6dbd6c14446918ae06a
nasimshishir <tech17196@gmail.com>
2022-10-26 02:33:06 +0600

    Folder Structer Ready

--- src/Layouts/Main.js
+++ src/Layouts/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../Pages/Shared/Header/Header';
 
 const Main = () => {
     return (
         <div>
-            <h2 className='#f8fafc'>Main Component</h2>
+            <Header></Header>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/Routes/Routes/routes.js
+++ src/Routes/Routes/routes.js
@@ -1,9 +1,46 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layouts/Main";
+import Blogs from "../../Pages/Blog/Blogs";
+import AllCourses from "../../Pages/Courses/All-Courses/AllCourses";
+import Faq from "../../Pages/FAQ/Faq";
+import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Shared/Login/Login";
+import Register from "../../Pages/Shared/Register/Register";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Main></Main>
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+
+            {
+                path: "/faq",
+                element: <Faq></Faq>
+            },
+
+            {
+                path: "/blogs",
+                element: <Blogs></Blogs>
+            },
+
+            {
+                path: "/courses",
+                element: <AllCourses></AllCourses>
+            },
+
+            {
+                path: "/login",
+                element: <Login></Login>
+            },
+
+            {
+                path: "/register",
+                element: <Register></Register>
+            }
+        ]
     }
 ])
